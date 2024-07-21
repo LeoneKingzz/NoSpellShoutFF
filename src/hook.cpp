e0761bb90db8460e28d005721adcbb22b8a4ee9b
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-07-21 05:48:46 +0100

    Update hook.cpp

--- src/hook.cpp
+++ src/hook.cpp
@@ -119,38 +119,4 @@ namespace hooks
 		return _ProcessEvent_NPC(a_sink, a_event, a_eventSource);
 	}
 
-	// EventResult on_animation_event::ProcessEvent_PC(RE::BSTEventSink<RE::BSAnimationGraphEvent>* a_sink, RE::BSAnimationGraphEvent* a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* a_eventSource)
-	// {
-	// 	ProcessEvent(a_sink, a_event, a_eventSource);
-	// 	return _ProcessEvent_PC(a_sink, a_event, a_eventSource);
-	// }
-
 }
-
-// const auto caster = a_defender->GetMagicCaster(RE::MagicSystem::CastingSource::kInstant);
-// float a_reprisal = (EldenParry::GetSingleton()->AttackerBeatsParry(a_aggressor, a_defender));
-// auto bHasEldenParryPerk2 = a_defender->HasPerk(RE::BGSPerk::LookupByEditorID("ORD_Bck20_TimedBlock_Perk_50_OrdASISExclude")->As<RE::BGSPerk>());
-// auto bHasEldenParryPerk1 = a_defender->HasPerk(RE::BGSPerk::LookupByEditorID("ORD_Bck20_TimedBlock_Perk_20_OrdASISExclude")->As<RE::BGSPerk>());
-// if (bHasEldenParryPerk2 || bHasEldenParryPerk1)
-// {
-//     RE::MagicItem *eldenArmorSpell = nullptr;
-//     if (bHasEldenParryPerk2 == true)
-//     {
-//         eldenArmorSpell = RE::TESForm::LookupByEditorID<RE::MagicItem>("ORD_Bck_TimedBlock_Spell_Proc_2");
-//     }
-//     else if (bHasEldenParryPerk1)
-//     {
-//         eldenArmorSpell = RE::TESForm::LookupByEditorID<RE::MagicItem>("ORD_Bck_TimedBlock_Spell_Proc");
-//     }
-//     caster->CastSpellImmediate(eldenArmorSpell, true, a_defender, 1, false, 45, a_defender);
-// }
-
-// const auto ActorTypeDragon = RE::BGSKeyword::LookupByEditorID("ActorTypeDragon")->As<RE::BGSKeyword>();
-// auto UDDragonAISpell = RE::TESForm::LookupByEditorID<RE::SpellItem>("aaaUDDragonAISpell");
-
-// bool IsActorTypeDragon = Dragon->HasKeyword(ActorTypeDragon);
-// bool HasUDDragonAISpell = Dragon->HasSpell(UDDragonAISpell);
-
-// uti/l::playSound(actor, RE::TESForm::LookupByEditorID<RE::TESForm>
-
-//Actors\\Character
