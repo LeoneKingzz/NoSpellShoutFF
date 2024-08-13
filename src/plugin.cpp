661d877719e4a4097924d6b344c3eea64a5ff2f1
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-08-13 12:44:01 +0100

    Update plugin.cpp

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -12,11 +12,15 @@ void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 	case SKSE::MessagingInterface::kDataLoaded:
 		break;
 	case SKSE::MessagingInterface::kPostLoad:
+		//hooks::util::install();
+		break;
+	case SKSE::MessagingInterface::kPostPostLoad:
+		hooks::util::install();
 		break;
 	case SKSE::MessagingInterface::kPreLoadGame:
 		break;
 	case SKSE::MessagingInterface::kPostLoadGame:
-		hooks::util::install();
+		//hooks::util::install();
 		break;
 	case SKSE::MessagingInterface::kNewGame:
 		break;
